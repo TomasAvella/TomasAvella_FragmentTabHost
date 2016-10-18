34800ddc836d4c4ef34fb2800f746ec72673d8da
Tomas Avella <tomasavella007@gmail.com>
2016-10-18 17:00:12 +0200

    Practica 2.12

---
+++ app/src/main/java/fragmenttabhost/example/net/fragmenttabhost/FragmentPestanya1.java
@@ -0,0 +1,31 @@
+package fragmenttabhost.example.net.fragmenttabhost;
+
+import android.support.v4.app.Fragment;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Tomas on 16/10/2016.
+ */
+
+public class FragmentPestanya1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.pestanya1_layout, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.text);
+        Resources res = getResources();
+        tv.setText(res.getString(R.string.eti_tab1));
+        return v;
+
+    }
+}
